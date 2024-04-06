6b465ed1fc9cd2a94b18308ce4a5536bb40bcc4c
LegendaV <115637903+LegendaV@users.noreply.github.com>
2024-04-06 19:52:53 +0500

    Add DataAccess

---
+++ DataAccess/Stacks.cs
@@ -0,0 +1,8 @@
+﻿namespace Mentorium.DataAccess
+{
+    public class Stacks
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ DataAccess/StudentInfoStack.cs
@@ -0,0 +1,10 @@
+﻿namespace Mentorium.DataAccess
+{
+    public class StudentInfoStack
+    {
+        public int StudentInfoId { get; set; }
+        public StudentInfo StudentInfo { get; set; }
+        public int StackId { get; set; }
+        public Stacks Stack { get; set; }
+    }
+}
